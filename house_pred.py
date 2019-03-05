e2cc6f6771f3d9fb97face3c0f4c7d05abaf0352
Apoorva Kumar .P <bpapoorva@gmail.com>
2019-03-05 19:30:28 +0530

    Update house_pred.py

--- house_pred.py
+++ house_pred.py
@@ -1,11 +1,4 @@
 
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Dec 18 20:20:30 2018
-
-@author: Koushik
-"""
-
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt
